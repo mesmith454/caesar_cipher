197dcbba5f62447725c58dc35786d1c2534367c6
mesmith454 <mesmith454@gmail.com>
2023-11-09 20:11:10 +0900

    reviewd and played with code to see what might break it or make it work better also added explanations

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,14 +1,17 @@
 def caesar_cipher(string, shift_factor)
-    # take each letter of string and convert to i
-    # shift
-    # convert back
-    # return new string
+    # take string and substitute letters [a-z] (pattern) with the following orders (replacement). on each letter do an operation
+        # somehow i within gsub argument prevents different letters from being assigned the same letter but in upcase for one and lowcase for other? need to look more into that
+    # order takes a letter and returns the position on ascii chart. checks upcase/lowcase
+    # original sets original values of string
+    # shifted moves position according to parameter passed to method, 26 because # of letters in alphabet (without it returns a text symbol instead of a letter)
+    #ciphered_letter converts numbered phrase back into letters
         string.gsub(/[a-z]/i) do |letter|
             order = (letter >= 'a' ? 'a' : 'A').ord
-            original = letter.ord - order
-            shifted = (original + shift_factor) % 26
-            (shifted + order).chr
+            original_letter = letter.ord - order
+            shifted_letter = (original_letter + shift_factor) % 26
+            ciphered_letter = (shifted_letter + order).chr
         end
 end
 
-p caesar_cipher("Hello World!", 3)
+# call method with arguments
+p caesar_cipher("Hello Biznatch!", 7)
